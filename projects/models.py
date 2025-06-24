d6ac393ed8fa7d1df5dc6658db37e9354e9bf907
ndl12ben <benweiss1@web.de>
2025-06-24 13:11:47 +0200

    test

--- projects/models.py
+++ projects/models.py
@@ -7,19 +7,14 @@ class Project(models.Model):
         'SI': 'Single-Label (Radio)',
         'MU': 'Multi-Label (Checkbox)'
     }
-
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-
     instructions = models.TextField(blank=True)
-
     label_type = models.CharField(
         max_length=2,
         choices=LABEL_TYPES
     )
-
     num_annotators = models.IntegerField(default=1)
-
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
