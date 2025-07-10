13370569476cf260b0e2127492b723976d3e8f12
Sophia <sophia.babl@stud.uni-regensburg.de>
2025-07-10 14:47:17 +0200

    Update zu Aufgabe 1 Labelverwaltung

--- annoteer/urls.py
+++ annoteer/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from annoteer.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
@@ -30,4 +32,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
