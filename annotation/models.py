ed577548830fc955e5bbec9b81a45ff600d4138b
Sophia <sophia.babl@stud.uni-regensburg.de>
2025-07-15 13:43:08 +0200

    Update annotation/models.py
    Noch nicht final gelöst

--- annotation/models.py
+++ annotation/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from dataitem.models import Dataitem
-from projects.models import Label
+from projects.models import Label, Project
 
 
 class Annotation(models.Model):
+    #project = models.ForeignKey(Project, on_delete=models.CASCADE)
     annotated_by = models.ForeignKey(User, related_name="annotations", on_delete=models.CASCADE)
     dataitem = models.ForeignKey(Dataitem, related_name="annotations" , on_delete=models.CASCADE)
 
